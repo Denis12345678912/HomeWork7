f0230e173c5334e55fe172118abd2b46469fb8ce
Den <no.den2018@yandex.ru>
2023-07-28 07:54:17 +0500

    Выполнили 50 задание

---
+++ task47/Program.cs
@@ -0,0 +1,37 @@
+﻿// 47.Задайте двумерный массив размером m*n,  заполненный случайными вещественными числами. (с плавающей точкой)
+double[,] Create2DMassiv(int rows, int columns)
+{
+    double [,] matrix = new double [rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().NextDouble() * 100;
+          matrix[i,j] =  Convert.ToDouble( matrix[i,j].ToString("0.00"));
+        }
+    }
+    return matrix;
+}
+
+int  GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Print2DMassiv(double [,] massiv)
+{
+ for(int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for(int j = 0; j < massiv.GetLength(1); j++)
+        {
+            Console.Write($"{massiv[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int rows = GetInput("Введите количество строк массива: ");
+int columns = GetInput("Введите количество столбцов массива: ");
+double [,] massiv = Create2DMassiv(rows,columns);
+Print2DMassiv(massiv);
---
+++ task50/Program.cs
@@ -0,0 +1,61 @@
+﻿// .Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет
+//строка 1  столбец 7 — такого числа в массиве нет
+//либо просто найти число в массиве, если оно есть
+
+
+int[,] Create2DMassiv(int rows, int columns)
+{
+    int [,] matrix = new int [rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(-99, 99);
+        }
+    }
+    return matrix;
+}
+
+int  GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Print2DMassiv(int [,] massiv)
+{
+ for(int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for(int j = 0; j < massiv.GetLength(1); j++)
+        {
+            Console.Write($"{massiv[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindNumber(int[,] array)
+{
+    Console.Write("Введите число, которое нужно найти: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+ for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if(number == array[i,j])
+           {
+             Console.WriteLine($"Число {number} находится на позиции [{i};{j}]");
+            return;
+           }
+        }
+        
+    }
+    Console.WriteLine($"Число {number} - в массиве нет");
+}
+
+int rows = GetInput("Введите количество строк массива: ");
+int columns = GetInput("Введите количество столбцов массива: ");
+int [,] massiv = Create2DMassiv(rows,columns);
+Print2DMassiv(massiv);
+ FindNumber(massiv);
